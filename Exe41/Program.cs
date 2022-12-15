dbcab5baa8b0db17a9d256b1653c637223e7ae86
Khalkina <tashik2010@mail.ru>
2022-12-15 12:23:54 +0300

    Решение задачи №41

---
+++ Exe41/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+
+//получение числа с консоли
+Console.Write("Введите элементы(через пробел): ");
+
+//получает количество положительных чисел
+int Count(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) count++;
+    }
+    return count;
+}
+
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+Console.WriteLine($"Количествово положительных чисел : {Count(arr)}");
+
+
